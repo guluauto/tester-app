b43fc0816bd64985c66ce380e21b14929bb44978
Kane <dmccer@gmail.com>
2015-06-27 15:22:27 +0800

    debug

--- debug.sh
+++ debug.sh
@@ -5,6 +5,6 @@ mv hooks/before_build hooks/before_build_bak
 cp -rf ../demigod-tester/dist/* ./www
 grep -rl "o.dp:3000" ./www | xargs sed -i '' 's/o.dp:3000/t.ifdiu.com/g'
 cordova build android --debug
-cordova run android --debug
 mv hooks/before_build_bak hooks/before_build
+cordova run android --debug
 exit
