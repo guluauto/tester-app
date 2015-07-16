b4a19ff276496db3e9846bbc2fed13cc03b7e19f
Kane <dmccer@gmail.com>
2015-07-16 17:22:57 +0800

    debug mod

--- hooks/after_build/upload.js
+++ hooks/after_build/upload.js
@@ -5,8 +5,10 @@ var shell = require('shelljs');
 upload();
 
 function upload() {
-  shell.cp('-rf', 'platforms/android/ant-build/MainActivity-release.apk', './');
-  shell.mv('./MainActivity-release.apk', './tester.apk');
-  console.log(shell.exec('scp ./tester.apk xyh@guluabc.com:/home/xyh/vermgr/public/tester.apk').output);
+  shell.cp('-rf', 'platforms/android/ant-build/MainActivity-debug.apk', './');
+  shell.mv('MainActivity-debug.apk', 'tester.apk');
+  var scp = shell.exec('scp ./tester.apk xyh@guluabc.com:/home/xyh/vermgr/public/tester.apk');
+  console.log(scp.output);
+  shell.exec('rm -rf ./tester.apk');
 }
 
