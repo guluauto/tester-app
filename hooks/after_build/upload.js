d339342742b16c465c0d1122b360330b81e171d7
Kane <dmccer@gmail.com>
2015-08-02 19:50:15 +0800

    pre & local

--- hooks/after_build/upload.js
+++ hooks/after_build/upload.js
@@ -7,7 +7,7 @@ upload();
 function upload() {
   shell.cp('-rf', 'platforms/android/ant-build/MainActivity-debug.apk', './');
   shell.mv('MainActivity-debug.apk', 'tester.apk');
-  var scp = shell.exec('scp ./tester.apk xyh@guluabc.com:/home/xyh/vermgr/public/tester.apk');
+  var scp = shell.exec('scp ./tester.apk xyh@guluabc.com:/home/xyh/app-getter/public/tester.apk');
   console.log(scp.output);
   shell.exec('rm -rf ./tester.apk');
 }
--- local.sh
+++ local.sh
@@ -5,12 +5,12 @@ mv hooks/before_build hooks/before_build_bak
 mv hooks/after_build hooks/after_build_bak
 
 cp -rf ../demigod-tester/dist/* ./www
-grep -rl "o.dp:3000" ./www | xargs sed -i '' 's/o.dp:3000/inspector.guluabc.com/g'
+grep -rl "o.dp:3000" ./www | xargs sed -i '' 's/o.dp:3000/192.168.31.34:8000/g'
 cordova build android --debug
 
 mv hooks/before_build_bak hooks/before_build
 mv hooks/after_build_bak hooks/after_build
 
 cordova run android --debug
-scp ./platforms/android/ant-build/MainActivity-debug.apk xyh@guluabc.com:/home/xyh/app-getter/public/tester-pre.apk
+scp ./platforms/android/ant-build/MainActivity-debug.apk xyh@guluabc.com:/home/xyh/app-getter/public/tester-local.apk
 exit
